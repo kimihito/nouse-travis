9c741172c7a90d0d4baa0229021e98f2d2a1d9f2
kimihito <tatsurotamashiro@gmail.com>
2013-11-19 17:29:07 +0900

    add secret_token.rb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,13 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+NouseTravis::Application.config.secret_key_base = '5ef8d28208ff1d65fe6d3afba41bd67eecc0d05243a4aa60c133c14d05e733a071e03c70dbb1014ca949974ed415db0fd84d2070a68d17b14f8ab4ccf15cdf78'
